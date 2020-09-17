a7f789fc40095a576bb8a6ff595aff7998200ec4
mrewers <mrewers@users.noreply.github.com>
2020-09-16 23:09:00 -0400

    feat: start outlining CLI operations

--- packages/create-lab-app/src/index.ts
+++ packages/create-lab-app/src/index.ts
@@ -1,25 +1,64 @@
 import { Command, flags as cmdFlags } from '@oclif/command';
 
+import { capitalize } from './utils/text';
+
 class GpaLabCreateLabApp extends Command {
-  static description = 'describe the command here';
+  static description =
+    'Bootstrap an app using the default GPA/LAB configurations.';
 
   static flags = {
     version: cmdFlags.version( { 'char': 'v' } ),
     help: cmdFlags.help( { 'char': 'h' } ),
-    name: cmdFlags.string( { 'char': 'n', description: 'name to print' } ),
-    force: cmdFlags.boolean( { 'char': 'f' } ),
+    bundler: cmdFlags.string(
+      {
+        'char': 'b',
+        description:
+          'identify the bundler that you would like to use for the project',
+        options: ['parcel', 'webpack'],
+        required: false,
+      },
+    ),
+    css: cmdFlags.string(
+      {
+        'char': 'c',
+        description:
+          'configure CSS type for the app, you can combine multiple options',
+        multiple: true,
+        options: [
+          'css',
+          'sass',
+          'modules',
+        ],
+        required: false,
+      },
+    ),
+    typescript: cmdFlags.boolean(
+      { 'char': 't', description: 'add TypeScript support to the project' },
+    ),
   };
 
-  static args = [{ name: 'file' }];
+  static args = [
+    {
+      name: 'project-directory',
+      required: true,
+      description: 'the name of of your application, we will create.',
+      hidden: false,
+    },
+  ];
 
   async run() {
     const { args, flags } = this.parse( GpaLabCreateLabApp );
 
-    const name = flags.name ?? 'world';
+    if ( args['project-directory'] ) {
+      this.log( args['project-directory'] );
+    }
 
-    this.log( `hello ${name} from ./src/index.ts` );
-    if ( args.file && flags.force ) {
-      this.log( `you input --force and --file: ${args.file}` );
+    if ( flags.bundler ) {
+      this.log( `Setting up a ${capitalize( flags.bundler )} project...` );
+    }
+
+    if ( flags.typescript ) {
+      this.log( 'typescript' );
     }
 
     return null;
